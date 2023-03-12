eb5b5da5f14f78956a68ea2fc597640dbe5a4c9d
JsilRyan <98880170+jsilryan@users.noreply.github.com>
2023-03-12 21:57:29 +0300

    Update Die.js

--- src/Die.js
+++ src/Die.js
@@ -1,9 +1,40 @@
 import React from "react"
 
 export default function Die(props) {
+    let bgcolor = props.isHeld ? "#59E391" : "#FFFFFF"
+    const styles = {
+        backgroundColor: bgcolor
+    }
+    function dots(){
+        let dot
+        dot = []
+        for (let i = 0; i < props.value; i++){
+            dot.push(".")
+        }
+        return dot
+    }
+    function sliceIntoChunks(arr, chunkSize) {
+        const res = [];
+        for (let i = 0; i < arr.length; i += chunkSize) {
+            const chunk = arr.slice(i, i + chunkSize);
+            res.push(chunk);
+        }
+        return res;
+    }
+    
+
     return (
-        <div className="die">
-            <h2>{props.value}</h2>
+        <div 
+            style={styles} 
+            className="die"
+            onClick={props.hold}
+        >
+            <h2 className="top">
+                {sliceIntoChunks(dots(), 3)[0]}
+            </h2>
+            <h2 className="lowera">
+                {sliceIntoChunks(dots(), 3)[1]}
+            </h2>
         </div>
     )
 }
